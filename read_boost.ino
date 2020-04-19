e05c0b1724029838d7ca4e3f6678906e8d403676
Andy Baran <andy.baran@hashicorp.com>
2020-04-19 09:15:04 -0400

    fixing comments

--- read_boost.ino
+++ read_boost.ino
@@ -3,9 +3,12 @@
  * Item model number  038906051C
  * Manufacturer Part Number  038906051C
  * OEM Part Number   0281002401, 038 906 051 C, 038906051C 
- * /
-
-
+ * 
+ * TODO : Figure out curve for temperature sensor and log air temp
+ * TODO : Integrate with OBDII code setup as another non-blocking task
+ * TODO : Make display prettier
+ * 
+ */
 
 #include <Wire.h>
 #include <SPI.h>
@@ -60,37 +63,3 @@ void loop() {
     }
     M5.update();
 }
-
-
-
-
-/* MISC Info on ESP32 Serial connections
- *  
- *  Baud-rates available: 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, or 115200, 256000, 512000, 962100
- *  
- *  Protocols available:
- * SERIAL_5N1   5-bit No parity 1 stop bit
- * SERIAL_6N1   6-bit No parity 1 stop bit
- * SERIAL_7N1   7-bit No parity 1 stop bit
- * SERIAL_8N1 (the default) 8-bit No parity 1 stop bit
- * SERIAL_5N2   5-bit No parity 2 stop bits 
- * SERIAL_6N2   6-bit No parity 2 stop bits
- * SERIAL_7N2   7-bit No parity 2 stop bits
- * SERIAL_8N2   8-bit No parity 2 stop bits 
- * SERIAL_5E1   5-bit Even parity 1 stop bit
- * SERIAL_6E1   6-bit Even parity 1 stop bit
- * SERIAL_7E1   7-bit Even parity 1 stop bit 
- * SERIAL_8E1   8-bit Even parity 1 stop bit 
- * SERIAL_5E2   5-bit Even parity 2 stop bit 
- * SERIAL_6E2   6-bit Even parity 2 stop bit 
- * SERIAL_7E2   7-bit Even parity 2 stop bit  
- * SERIAL_8E2   8-bit Even parity 2 stop bit  
- * SERIAL_5O1   5-bit Odd  parity 1 stop bit  
- * SERIAL_6O1   6-bit Odd  parity 1 stop bit   
- * SERIAL_7O1   7-bit Odd  parity 1 stop bit  
- * SERIAL_8O1   8-bit Odd  parity 1 stop bit   
- * SERIAL_5O2   5-bit Odd  parity 2 stop bit   
- * SERIAL_6O2   6-bit Odd  parity 2 stop bit    
- * SERIAL_7O2   7-bit Odd  parity 2 stop bit    
- * SERIAL_8O2   8-bit Odd  parity 2 stop bit    
-*/
